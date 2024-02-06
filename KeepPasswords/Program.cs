acdf394dd360cfe98a698e89f23a28d08954320c
astarte19 <yarochkin.artem@yandex.ru>
2024-02-06 22:32:58 +0300

    Фикс

--- KeepPasswords/Program.cs
+++ KeepPasswords/Program.cs
@@ -43,6 +43,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=PasswordsKeeper}/{action=Index}/{id?}");
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
