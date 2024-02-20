bb100ad2d19cc40a879a78274bad43f9a976ebd2
astarte19 <yarochkin.artem@yandex.ru>
2024-02-20 22:20:39 +0300

    Branch with SQLServer DB Provider

--- KeepPasswords/Program.cs
+++ KeepPasswords/Program.cs
@@ -7,7 +7,7 @@ using System.Configuration;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<ApplicationContext>(options =>
-               options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+               options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddIdentity<User, IdentityRole>(
     opts =>
